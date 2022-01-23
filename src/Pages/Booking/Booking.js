fce5eec524252f4f52b7416407d9a03c7a72fe6b
Salemah <kadertanvir1@gmail.com>
2022-01-23 23:46:55 +0600

    packege updated

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React, { useEffect, useState, useRef } from "react";
+import { Col, Container, Row } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router";
+import './Booking.css';
 
 const Booking = () => {
-    return (
-        <div>
-                        <h1>Hello</h1>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, natus nesciunt distinctio unde ab est temporibus suscipit obcaecati dignissimos ipsam tenetur ducimus officia vel in nisi exercitationem labore, illo necessitatibus error quaerat recusandae vero architecto, adipisci alias! Iusto, dolore eos provident corporis aperiam velit nemo aliquam debitis dignissimos repellendus in?</p>
+  const { id } = useParams();
+  const [service, setService] = useState({});
+  const [booking, setBooking] = useState({});
+  const { register, handleSubmit,reset, watch, formState: { errors } } = useForm();
+
+
+
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/packege/${id}`)
+      .then(res => res.json())
+      .then(data => setService(data))
+  }, []);
+  const onSubmit = data => 
+{
+  
+  fetch(`http://localhost:5000/booking`, {
+     method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+       .then((res) => res.json())
+      .then((result) => {
+         console.log(result);
+        if (result.insertedId) {
+           alert("order processed successfully");
+            reset();
+        }
+      });
+   
+
+  //     e.preventDefault();
+};
+
+  return (
+
+ <div class="bb">
+      <Container className="mt-5  ">
+      <Row className="conatilener-box ">
+        <Col sm={4}>
+          <div className="border p-3">
+            <h3>Packege Description</h3>
+            <img className="img-fluid" src={service.img} alt="" />
+            <h3>Title: {service.name}</h3>
+            <p>Description: {service.description}</p>
+            <h5>Price: ${service.price}</h5>
+          </div></Col>
+        <Col sm={8}><div className=" p-3 ">
+          <h2>Please Confirm Your Booking</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+           
+            <input defaultValue={service.name} {...register("name")} /><br/>
+            <input defaultValue={service.price} {...register("price")} /><br/>
+            <input defaultValue="test" {...register("username")} /><br/>
+            <input defaultValue="test" {...register("email")} /><br/>
+            <input placeholder="Address" {...register("address")} /><br/>
+            <input placeholder="Number"{...register("phone")} /><br/>
+
+            <input className="btn btn-primary" type="submit" />
+          </form>
+
         </div>
-    );
+        </Col>
+      </Row>
+
+    </Container>
+ </div>
+
+
+  );
 };
 
 export default Booking;
+
