08f0fef2bf21cd8bdb42f4cd363babbc4050da7d
Salemah <kadertanvir1@gmail.com>
2022-01-25 11:51:59 +0600

    mybooking updated

--- src/Pages/Dashboard/Mybooking/Mybooking.js
+++ src/Pages/Dashboard/Mybooking/Mybooking.js
@@ -10,14 +10,42 @@ const Mybooking = () => {
     useEffect(() => {
         fetch(`https://sleepy-plains-48362.herokuapp.com/mybooking/${user.email}`)
             .then(res => res.json())
-            .then(data => console.log(data))
-    }, [user.email])
+            .then(data => setBooking(data))
+    }, []);
     return (
         <div className='bb'>
-           <p>{booking.price
-}</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sed nobis repudiandae recusandae vero praesentium magni odio, sequi architecto officiis.</p>
-            <p>{booking.length}</p>
+           <table class="table table-striped mybooking">
+                <thead>
+                    <tr >
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Packege</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        booking.map(book => <tr key={book._id}>
+                            <td>{book.username}</td>
+                            <td>{book.email}</td>
+                            <td>{book.name}</td>
+                            <td>{book.price}</td>
+                            <td>{book.phone}</td>
+                            <td>{book.status}</td>
+
+                        </tr>)
+                    }
+                   
+
+
+
+
+                </tbody>
+            </table>
+
         </div>
     );
 };
